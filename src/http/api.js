8ef9f505093c7ce3796d72155dc076e201a8381d
mila <milarabello.dias@gmail.com>
2020-07-14 15:39:10 +0100

    add documentation

--- src/http/api.js
+++ src/http/api.js
@@ -1,3 +1,9 @@
+
+/**
+ * Function with external api request
+ * @param {string} query the input value of the search
+ * @param {func} setMovieList set state with request response
+ */
 export const apiFetch = async (query, setMovieList) => {
   const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_MOVIE_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
   try {

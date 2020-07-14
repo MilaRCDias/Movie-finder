87db578ad2334a2a67d778dd6a7ef73221d0d593
mila <milarabello.dias@gmail.com>
2020-07-14 15:12:47 +0100

    directory structured

---
+++ src/http/api.js
@@ -0,0 +1,11 @@
+export const apiFetch = async (query, setMovieList) => {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_MOVIE_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.log(data.results)
+    setMovieList(data.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
